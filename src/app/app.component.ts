0475080afdcfc4ceacb7a1ec74ed9fe734d1f3c0
szafranic <ryan.szafranic@gmail.com>
2022-11-11 16:02:25 -0500

    styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToDo } from 'src/todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ToDo';
+
+  todoList: ToDo[] = [
+	{task:"take vitamin",completed:false},
+	{task:"sleep",completed:false},
+	{task:"sleep more",completed:false},
+	{task:"eat",completed:true},
+	{task:"wow",completed:false}
+  ];
+
+  completeTask(index:number){
+	this.todoList[index].completed = true;
+  }
+
+  addTask(){
+	let newTask:string = (<HTMLInputElement> document.getElementById("NewItem")).value;
+	this.todoList.push({task:newTask, completed:false});
+	(<HTMLInputElement> document.getElementById("NewItem")).value = '';
+}	
+
+  mouseGreen(index:number){
+	
+  }
+
 }
